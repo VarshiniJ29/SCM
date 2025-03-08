804d2e59848887a98180f283df6aa627e4bd9cba
VarshiniJ29 <varshini.j@s.amity.edu>
2025-03-08 13:53:31 +0530

    found largest of three numbers

--- largest.c
+++ largest.c
@@ -8,5 +8,16 @@ int main() {
 	printf("enter the third number:");
 	scanf("%d",&num3);
 
-	
+    if(num 1 >= num 2 && num 1 >= num 3){
+      printf("The largest number is%d\n:",num1);
+    } else if(num 2>= num 1 && num 2>= num 3){
+	    printf("the largest numnber is%d\n:",num2){
+	    }else {
+		    printf("the largest number is %d\n:",num3);
+	    }
+	    return 0;
+    }
+			   
+
+		    
 		
